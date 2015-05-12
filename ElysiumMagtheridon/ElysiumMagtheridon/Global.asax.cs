ebba72aa986004bf9d7df8989581f1d1498196ab
kienlebr <kienlebr@gmail.com>
2015-05-11 20:14:37 -0400

    Revert "Home & Progression"
    This reverts commit 743d9ae901f441abc8fa87f28383626834b5d23c.

--- ElysiumMagtheridon/ElysiumMagtheridon/Global.asax.cs
+++ ElysiumMagtheridon/ElysiumMagtheridon/Global.asax.cs
@@ -4,13 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Optimization;
-using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.Configuration;
-using System.Configuration;
-using System.ComponentModel;
-using ElysiumMagtheridon.Models;
-using ElysiumMagtheridon.Controllers;
 
 namespace ElysiumMagtheridon
 {
@@ -20,15 +13,6 @@ namespace ElysiumMagtheridon
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            BundleTable.EnableOptimizations = false;
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
-
-            IUnityContainer container = new UnityContainer().LoadConfiguration(section, "Unity");
-
-            var that = this;
         }
     }
 }
