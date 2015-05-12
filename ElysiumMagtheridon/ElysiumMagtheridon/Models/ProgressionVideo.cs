1cf4c2ded68e34a43c3eb932316ab91d708ad89b
kienlebr <kienlebr@gmail.com>
2015-05-11 20:15:24 -0400

    Revert "Revert "Home & Progression""
    This reverts commit ebba72aa986004bf9d7df8989581f1d1498196ab.

---
+++ ElysiumMagtheridon/ElysiumMagtheridon/Models/ProgressionVideo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElysiumMagtheridon.Models
+{
+    public class ProgressionVideo : IProgressionItem
+    {
+        string type;
+
+        public ProgressionVideo(string videoType)
+        {
+            this.type = videoType;
+        }
+    }
+}
